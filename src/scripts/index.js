5eb6d4ea576ba2746e9d57d04ef41f6f3731a29e
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-08-29 16:17:35 +0500

    created static UI using html,css

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,43 +2,43 @@ import "normalize.css";
 import "../styles/reset.css";
 import "../styles/style.css";
 
-let path =
-	"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/[location]/[date1]/[date2]?key=YOUR_API_KEY ";
 let city = "peshawar";
 let apiKey = "JXY5BP8S4JSLUGDSRP6793223";
 let address = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=${apiKey}`;
 
 function fetchWeatherData(url) {
-	return new Promise((resolve, reject) => {
-		fetch(url)
-			.then((response) => {
-				if (!response.ok) {
-					throw new Error(
-						`HTTP Error: ${response.status} - ${response.statusText}`
-					);
-				}
-				return response.json();
-			})
-			.then((data) => {
-				resolve(data.currentConditions);
-			})
-			.catch((error) => reject(error));
+	return fetch(url).then((response) => {
+		if (!response.ok) {
+			throw new Error(
+				`HTTP Error: ${response.status} - ${response.statusText}`
+			);
+		}
+		return response.json();
 	});
 }
 
 function requiredWeatherData() {
 	return fetchWeatherData(address).then((data) => {
 		return {
-			temp: data.temp,
-			feelslike: data.feelslike,
-			conditions: data.conditions,
+			address: data.address,
+			resolvedAddress: data.resolvedAddress,
+			currentConditions: {
+				temperature: data.currentConditions.temp,
+				feelslike: data.currentConditions.feelslike,
+				conditions: data.currentConditions.conditions,
+				humidity: data.currentConditions.humidity,
+				sunrise: data.currentConditions.sunrise,
+				sunset: data.currentConditions.sunset,
+				datetime: data.currentConditions.datetime,
+			},
 		};
 	});
 }
-requiredWeatherData()
+/* requiredWeatherData()
 	.then((data) => {
-		console.log(data);
+		console.log(data.currentConditions);
 	})
 	.catch((error) => {
 		console.error(`Error fetching weather data: ${error}`);
 	});
+ */
