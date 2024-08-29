a5743f7cbf00bc8c598c67b0618e0c03c015c50f
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-08-29 09:43:17 +0500

    created function that fetches weather data using api

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,4 +2,44 @@ import "normalize.css";
 import "../styles/reset.css";
 import "../styles/style.css";
 
-console.log("Hello Webpack!");
+let path =
+	"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/[location]/[date1]/[date2]?key=YOUR_API_KEY ";
+let city = "peshawar";
+let apiKey = "JXY5BP8S4JSLUGDSRP6793223";
+let address = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=${apiKey}`;
+
+function fetchWeatherData(url) {
+	return new Promise((resolve, reject) => {
+		fetch(url)
+			.then((response) => {
+				if (!response.ok) {
+					throw new Error(
+						`HTTP Error: ${response.status} - ${response.statusText}`
+					);
+				}
+				return response.json();
+			})
+			.then((data) => {
+				resolve(data.currentConditions);
+			})
+			.catch((error) => reject(error));
+	});
+}
+/* 
+function requiredWeatherData() {
+	return fetchWeatherData(address).then((data) => {
+		return {
+			temp: data.temp,
+			feelslike: data.feelslike,
+			conditions: data.conditions,
+		};
+	});
+}
+requiredWeatherData()
+	.then((data) => {
+		console.log(data);
+	})
+	.catch((error) => {
+		console.error(`Error fetching weather data: ${error}`);
+	});
+ */
