68bec125b97e2700a3dd0c1482c9f9ae8dc5b301
Shahid Afridi <130848915+shahidafridi-321@users.noreply.github.com>
2024-09-03 20:56:17 +0500

    Remove unnecessary code fetch-weather-data.js

--- src/scripts/fetch-weather-data.js
+++ src/scripts/fetch-weather-data.js
@@ -1,17 +1,5 @@
 import { errorHandler } from "./DOMController.js";
 
-/* export function fetchWeatherData(url) {
-	return fetch(url).then((response) => {
-		if (!response.ok) {
-			throw new Error(
-				`HTTP Error: ${response.status} - ${response.statusText}`
-			);
-		}
-		return response.json();
-	});
-}
- */
-
 // Fetches weather data from the provided API URL and returns the JSON response
 
 export async function fetchWeatherData(url) {
