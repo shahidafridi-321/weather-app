960fa0b05a48c914125e38feae9e80fe98c79b64
Shahid Afridi <130848915+shahidafridi-321@users.noreply.github.com>
2024-09-03 20:57:37 +0500

    Remove unnecessary code from required-weather-data.js

--- src/scripts/required-weather-data.js
+++ src/scripts/required-weather-data.js
@@ -1,31 +1,6 @@
 import { fetchWeatherData } from "./fetch-weather-data.js";
 import { errorHandler } from "./DOMController.js";
 
-/* export function requiredWeatherData(location) {
-	let apiKey = "QC3XC9RCAZP8JNSJGLCVFBUDR";
-	let address = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${apiKey}`;
-
-	return fetchWeatherData(address).then((data) => {
-		return {
-			description: data.days[0].description,
-			address: data.address,
-			resolvedAddress: data.resolvedAddress,
-			currentConditions: {
-				temperature: data.currentConditions.temp,
-				feelslike: data.currentConditions.feelslike,
-				icon: data.currentConditions.icon,
-				conditions: data.currentConditions.conditions,
-				humidity: data.currentConditions.humidity,
-				sunrise: data.currentConditions.sunrise,
-				sunset: data.currentConditions.sunset,
-				datetime: data.currentConditions.datetime,
-				precipprob: data.days[0].precipprob,
-				wind: data.currentConditions.windspeed,
-			},
-		};
-	});
-} */
-
 // Fetches weather data from the Visual Crossing API for the specified location and returns an object with key weather details
 
 export async function requiredWeatherData(location) {
